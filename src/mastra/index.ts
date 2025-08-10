391ee084cccec36686a72a40df8f5fe886a64804
maqingjie <wb_maqingjie@meituan.com>
2025-08-10 19:19:59 +0800

    Initial commit: Add Mastra weather app

---
+++ src/mastra/index.ts
@@ -0,0 +1,29 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
+
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+  deployer: new CloudflareDeployer({
+    env: {
+      CLOUDFLARE_ACCOUNT_ID: "70e4bf0f3e51fe1e4f5a7eb2f5b00a1a",
+      CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN!,
+      CLOUDFLARE_API_EMAIL: "Maqingjie646@gmail.com"
+    },
+    projectName: "hello-mastra"
+  })
+});
